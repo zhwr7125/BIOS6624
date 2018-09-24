df19864f484b62c5ea839d8f65eec9a863c146d5
Wenru Zhou <zhwr7125@gmail.com>
2018-09-23 23:00:59 -0600

    Revise on some calculation

--- Project1/Code/Tables.R
+++ Project1/Code/Tables.R
@@ -2,7 +2,12 @@ source('DataClean.R')
 
 
 #Combine unknowns as missing
-#BMI
+#BMI missing and strange value
+#strange, Improbable value, missing
+#see values
+data_wide$BMI_0[data_wide$BMI_0>500]
+#see summary
+unknown_bmi<-c(length(data_wide$BMI_0[data_wide$BMI_0>500]),length(data_wide$BMI_0[data_wide$BMI_0==-1]),length(data_wide$BMI_0[is.na(data_wide$BMI_0)==TRUE]))
 unknown_bmi_count<-table(data_wide$BMI_0>500|data_wide$BMI_0==-1|is.na(data_wide$BMI_0)==TRUE)[2]
 unknown_bmi_rate<-table(data_wide$BMI_0>500|data_wide$BMI_0==-1|is.na(data_wide$BMI_0)==TRUE)[2]/(table(data_wide$BMI_0>500|data_wide$BMI_0==-1|is.na(data_wide$BMI_0)==TRUE)[1]+table(data_wide$BMI_0>500|data_wide$BMI_0==-1|is.na(data_wide$BMI_0)==TRUE)[2])
 data_wide$BMI_0[data_wide$BMI_0>500|data_wide$BMI_0==-1]<-NA
@@ -10,20 +15,25 @@ data_wide$BMI_0[data_wide$BMI_0>500|data_wide$BMI_0==-1]<-NA
 #max(data_wide$BMI_0,na.rm=TRUE)
 
 #income
+unknown_income<-c(length(data_wide$income_0[data_wide$income_0=="Do not wish to answer"]),length(data_wide$income_0[is.na(data_wide$income_0)==TRUE]))
 unknown_income_count<-table(data_wide$income_0=="Do not wish to answer"|is.na(data_wide$income_0)==TRUE)[2]
 unknown_income_rate<-table(data_wide$income_0=="Do not wish to answer"|is.na(data_wide$income_0)==TRUE)[2]/(table(data_wide$income_0=="Do not wish to answer"|is.na(data_wide$income_0)==TRUE)[1]+table(data_wide$income_0=="Do not wish to answer"|is.na(data_wide$income_0)==TRUE)[2])
 data_wide$income_0[data_wide$income_0=="Do not wish to answer"]<-NA
 
 #table 0: Missing at baseline
 var0<-c("AGG_MENT_0","AGG_PHYS_0","LEU3N_0","VLOAD_0","group","age_0","BMI_0","RACE_0","income_0","SMOKE_0")
+label(data_wide$years_0)<-c("Baseline")
+levels(data_wide$years_0)<-c("Baseline")
 con_missing0<-missing_table(data_wide,var0,data_wide$years_0)
 
 #table 0: Missing at 2
 var2<-c("AGG_MENT_2","AGG_PHYS_2","LEU3N_2","VLOAD_2","ADH_2")
+label(data_wide$years_2)<-c("Vist 2")
+levels(data_wide$years_2)<-c("Visit 2")
 con_missing2<-missing_table(data_wide,var2,data_wide$years_2)
 
 #Those who have BMI missing, insufficient and improbable should not be included in this model
-data_final<-subset(data_wide,is.na(data_wide$BMI)==FALSE)
+data_final<-subset(data_wide,is.na(data_wide$BMI)==FALSE&is.na(data_wide$income_0)==FALSE)
 
 #table 1 at baseline and yr2
 var1_0<-c("AGG_MENT_0","AGG_PHYS_0","LEU3N_0","VLOAD_0","age_0","BMI_0","RACE_0","income_0","SMOKE_0")
