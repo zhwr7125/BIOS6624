5454be51f9c41c122cf887ddc39efec1bd8c5f0f
Wenru Zhou <zhwr7125@gmail.com>
2018-10-21 17:34:06 -0600

    fix the variance and add some mixture part

--- Project2/Code/data_simu.R
+++ Project2/Code/data_simu.R
@@ -38,7 +38,9 @@ X <- model.matrix(~ grp*time) # reference cell coding
 id <- rep(1:n, each = p) # id indexing clusters
 
 # generate correlated random intercept
-# Skewed: b0i+1~logN(0,3) b1+1~logN(0,2) or b1+1~logN(0,0)
+# Skewed: 
+#b0i+1~logN(0,4) b1+1~logN(0,0)
+#b0i+1~logN(0,3) b1+1~logN(0,2)
 Sig0 <- gmat(sig2 = c(sqrt(var_b0),sqrt(var_b1)), rho = 0, p = 2)
 d <- mvrnorm(n, mu = c(0,0), Sigma = Sig0)
 b0 <- exp(d[,1])-exp(sqrt(var_b0)/2)
@@ -51,10 +53,11 @@ e<-rnorm(n*p,0,sqrt(3))
 
 #create outcome
 y <- c( X %*% beta + d0 + d1*time + e ) # linear predictor
+y_fix <- c( X %*% beta )
 #y <- c( X %*% beta) # linear predictor for check
 
 
-sample<-data.frame(id,y,grp,time)
+sample<-data.frame(id,y,y_fix,grp,time,d0,d1,e)
 
 }
 
@@ -73,16 +76,38 @@ for (i in 1:length(size)){
   }
 }
 
-type_n<-c(0,2)
+
   for (i in 1:length(size)){
     for (j in 1:length(time)){
-        for (l in 1:length(type_n)){
-          assign(paste(haha[i,j,1,l], sep=""), data_gene(n1=size[i]/2,n2=size[i]/2,p=time[j],beta=c(0, 0, 0, 0),var_b0=3,var_b1=type_n[l]))
-          assign(paste(haha[i,j,2,l], sep=""), data_gene(n1=size[i]/2,n2=size[i]/2,p=time[j],beta=c(2, 1, 0.5, 0.25),var_b0=3,var_b1=type_n[l]))
-          
-        }
+          assign(paste(haha[i,j,1,1], sep=""), data_gene(n1=size[i]/2,n2=size[i]/2,p=time[j],beta=c(0, 0, 0, 0),var_b0=4,var_b1=0))
+          assign(paste(haha[i,j,2,1], sep=""), data_gene(n1=size[i]/2,n2=size[i]/2,p=time[j],beta=c(2, 1, 0.5, 0.25),var_b0=4,var_b1=0))
+          assign(paste(haha[i,j,1,2], sep=""), data_gene(n1=size[i]/2,n2=size[i]/2,p=time[j],beta=c(0, 0, 0, 0),var_b0=3,var_b1=2))
+          assign(paste(haha[i,j,2,2], sep=""), data_gene(n1=size[i]/2,n2=size[i]/2,p=time[j],beta=c(2, 1, 0.5, 0.25),var_b0=3,var_b1=2))
     }
   }
 
 rand.fit <- lmer(y ~ grp*time + (1 + time|id),data=Skew20_t5_alter_slp) # using lme4 package
 summary(rand.fit)
+
+
+
+##################Mixture part#############
+
+guagua<-Skew20_t5_alter_slp[c(1,3,4,5,8)]
+uni_id<-rep(runif(20,0,1),each=5)
+guagua2<-data.frame(guagua,uni_id)
+guagua2$indicat[guagua2$uni_id<=0.3] <- 0
+guagua2$indicat[guagua2$uni_id>0.3] <- 1
+
+#random intercept
+Sig1 <- gmat(sig2 = c(3,0), rho = 0, p = 2)
+Sig2 <- gmat(sig2 = c(6,0), rho = 0, p = 2)
+
+d_1.3 <- mvrnorm(20, mu = c(0,0), Sigma = Sig1)
+d_2.3 <- mvrnorm(20, mu = c(0,0), Sigma = Sig2)
+
+b0 <- (1/3)*d_1.3[,1]+(2/3)*d_2.3[,1]
+b1 <- (1/3)*d_1.3[,2]+(2/3)*d_2.3[,2]
+d0 <-rep(b0,each=5)
+d1 <-rep(b1,each=5)
+
